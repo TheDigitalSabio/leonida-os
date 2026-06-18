ea3fce64867bff963d550076f7cf0a183f3ab0c5
DG <dimitri.gordon93@gmail.com>
2026-06-18 09:32:58 -0700

    feat: initial clean basline for Leonida OS

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div style={{ 
+      width: '100vw', 
+      height: '100vh', 
+      backgroundColor: '#0f172a', 
+      color: '#f8fafc',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Leonida OS Terminal Loading...</h1>
+    </div>
+  );
+}
+
+export default App;
